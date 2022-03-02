c017ae805036b01c2ff6f7b53bbdad171d4752b6
c2tm <cyork4839@gmail.com>
2022-03-02 16:31:07 -0500

    updates serializers import

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 
-from . import serializersArticleSerializer
+from . import ArticleSerializer
 from .models import Article
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from articles.permissions import IsUserOrReadOnly, IsAdminOrReadOnly, UserPermsDetail, IsAdminOnly
